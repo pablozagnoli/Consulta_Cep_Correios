3206c02152998f2a879260e007c550a18fbfb1e0
Pablo Zagnoli <68761353+pablozagnoli@users.noreply.github.com>
2021-08-11 01:09:33 -0300

    Add files via upload

---
+++ Consulta_Cep_Correios/Consulta_Cep_Correios/Models/Model_Reques_Header.cs
@@ -0,0 +1,11 @@
+﻿namespace Consulta_Cep_Correios.Models
+{
+    public class Model_Reques_Header
+    {
+        public string pagina { get; set; }
+        public string cepaux { get; set; }
+        public string mensagem_alerta { get; set; }
+        public string endereco { get; set; }
+        public string tipoCEP { get; set; }
+    }
+}
---
+++ Consulta_Cep_Correios/Consulta_Cep_Correios/Models/Models_Response_Cep.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Consulta_Cep_Correios.Models
+{
+    class Models_Response_Cep
+    {
+        public List<Dadoscep> Dados { get; set; }
+    }
+    class Dadoscep 
+    {
+        public string uf { get; set; }
+        public string localidade { get; set; }
+        public string locNoSem { get; set; }
+        public string locNu { get; set; }
+        public string localidadeSubordinada { get; set; }
+        public string logradouroDNEC { get; set; }
+        public string logradouroTextoAdicional { get; set; }
+        public string logradouroTexto { get; set; }
+        public string bairro { get; set; }
+        public string baiNu { get; set; }
+        public string nomeUnidade { get; set; }
+        public string cep { get; set; }
+        public string tipoCep { get; set; }
+        public string numeroLocalidade { get; set; }
+        public string situacao { get; set; }
+    }
+}
