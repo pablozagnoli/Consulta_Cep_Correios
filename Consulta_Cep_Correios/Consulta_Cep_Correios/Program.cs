89cf03768eaf1a21e5b4682e011c9c5b6b150920
Pablo Zagnoli <68761353+pablozagnoli@users.noreply.github.com>
2021-08-11 01:20:08 -0300

    Add files via upload

---
+++ Consulta_Cep_Correios/Consulta_Cep_Correios/Program.cs
@@ -0,0 +1,13 @@
+﻿using Consulta_Cep_Correios.Services;
+
+
+namespace Consulta_Cep_Correios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Request.Request_Cep();
+        }
+    }
+}
